21b046df8775511a9da16854ac6880e2ea50ca1d
0xdblok <0xdblok@users.noreply.github.com>
2026-06-20 15:27:26 +0000

    Force dynamic garage page for dev tools env var

--- src/app/garage/page.tsx
+++ src/app/garage/page.tsx
@@ -1,5 +1,7 @@
 import { WalletGameDashboard } from "@/components/WalletGameDashboard";
 
+export const dynamic = "force-dynamic";
+
 export default function GaragePage() {
   return <WalletGameDashboard />;
 }
